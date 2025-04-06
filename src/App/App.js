8c8aa8da34d8e53ff47e088d9e5a1a1084113d38
Beverly Green <greenbev000@gmail.com>
2025-04-06 18:03:34 -0500

    imported home icon and movie details to app to pass as props to movie details, created outline of function for displaying movie details view

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import searchIcon from '../icons/search.png';
 import upvoteIcon from '../icons/upvote.png';
 import downvoteIcon from '../icons/downvote.png';
+import homeIcon from '../icons/home.png';
 
 // Example imports (for later):
 import { useState, useEffect } from 'react';
 import moviePosters from '../data/movie_posters';
-// import movieDetails from '../data/movie_details';
+import movieDetails from '../data/movie_details';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
 
   const [posters, setPosters] = useState(moviePosters)
+  const [details, setDetails] = useState(movieDetails)
 
   function upvote(id){
     // code that says increase vote count
@@ -40,7 +42,10 @@ function App() {
     setPosters([...posters])
   };
 
-
+  function displayDetails(id) {
+    setDetails([...details])
+    //not sure if i should pass id yet since all moviedetails are the same for now, i think we will need it for fetching real data
+  }
 
   return (
     <main className='App'>
@@ -48,6 +53,7 @@ function App() {
         <h1>rancid tomatillos</h1>
       </header>
         <MoviesContainer posters={posters} upvote={upvote} downvote={downvote} upvoteIcon={upvoteIcon} downvoteIcon={downvoteIcon}/>
+        <MovieDetails details={details} displayDetails={displayDetails} homeIcon={homeIcon}/>
     </main>
   );
 }
