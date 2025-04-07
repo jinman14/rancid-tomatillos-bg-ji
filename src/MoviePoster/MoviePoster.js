ab95196bceb6d6240dc763ee3fc3fb0227b1e227
Beverly Green <greenbev000@gmail.com>
2025-04-06 20:08:18 -0500

    passed displayDetails function as prop to Movie poster so it can be triggered when clicked

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,9 +1,10 @@
 import './MoviePoster.css';
 
-function MoviePoster({id, image, title, vote_count, upvote, downvote, upvoteIcon, downvoteIcon}) {
+function MoviePoster({id, image, title, vote_count, upvote, downvote, upvoteIcon, downvoteIcon, displayDetails}) {
   return (
     <div className='MoviePoster'>
-      <img className = "MoviePosterImg" src = {image}/>
+      <img className = "MoviePosterImg" src = {image}
+      onClick={() => displayDetails() }/>
       <h3><button onClick={() => upvote(id)}><img className = "VoteButton" src={upvoteIcon}/></button> {vote_count}<button onClick={() => downvote(id)}><img className = "VoteButton" src={downvoteIcon}/></button></h3>
     </div>
   );
