a7c72533cf7a404c98080151d413f29629b54061
Beverly Green <greenbev000@gmail.com>
2025-04-06 20:07:14 -0500

    pulled out movie details from data object, added HTMl to format them on page, have not touched css styling yet

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,9 +1,25 @@
 import './MovieDetails.css';
 
-function MovieDetails() {
+function MovieDetails({details, homeIcon, goHome}) {
+  const backdrop_path = details.backdrop_path
+  const genre_ids = details.genre_ids
+  const overview = details.overview
+  const title = details.title
+
+  const genres = genre_ids.join(', ');
+
   return (
     <section className='MovieDetails'>
-      <p>Movie Details go here!</p>
+      <div className='MovieBackdrop'>
+        <img src={backdrop_path} />
+      </div>
+      <h2> {title} </h2>
+      <h3> {genres} </h3>
+      <p> {overview} </p>
+      <button onClick={goHome} className="home-button">
+        <img src={homeIcon} alt="Home Icon" />
+        Back to Movie List
+      </button>
     </section>
   );
 }
