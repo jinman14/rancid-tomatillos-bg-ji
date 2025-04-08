31c42d7b10201bebd0351fa79320daaf0db317a8
Beverly Green <greenbev000@gmail.com>
2025-04-08 17:08:51 -0500

    imported posters to main page cypress testing

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,5 @@
-// Mock data to use for testing:
-// import posters from '../fixtures/movie_posters.json' (we've added mock data to this file for you!)
+
+import posters from '../fixtures/movie_posters.json' 
 // import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
 
 describe('Main Page', () => {
