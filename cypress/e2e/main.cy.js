d5240134f12636fbf8f777dc14395e286d4578b7
Beverly Green <greenbev000@gmail.com>
2025-04-08 17:31:00 -0500

    added testing for displayed posters on main page on page load

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -3,10 +3,25 @@ import posters from '../fixtures/movie_posters.json'
 // import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
 
 describe('Main Page', () => {
-  it('displays title on page load', () => {
-    // hint: you'll want to add an intercept here if you are making a network request on page load!
+  beforeEach(() => {
+    cy.intercept("GET", "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies", {
+      statusCode: 200,
+      fixture: "movie_posters" 
+    })
     cy.visit('http://localhost:3000/')
-    .get('h1')
-    .contains('rancid tomatillos')
+  })
+
+  it('displays title on page load', () => {
+    cy.get('h1')
+    cy.contains('rancid tomatillos')
+  })
+
+  it('displays all the movie posters on page load', () => {
+    cy.get('.MoviesContainer').should('exist')
+    cy.get('.MoviePoster').should('exist')
+    cy.get('.MoviePosterImg').should('exist')
+    cy.get('.VoteButton').should('exist')
+    cy.get('.MoviePoster').should('have.length', 4)
+    cy.get('.MoviePoster').first().find('button').should('exist')
   })
 })
