eb1b9668087a351a0ac5b75b6c33155fd6feb973
Jacob Inman <jrinman6295@gmail.com>
2025-04-09 10:50:57 -0700

    took vote count out of link block

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -7,8 +7,8 @@ function MoviePoster({id, image, title, vote_count, castVote, upvoteIcon, downvo
       <Link to={`/${id}`}>
         <img className = "MoviePosterImg" src = {image}
         onClick={() => displayDetails(id) }/>
-        <h3><button onClick={() => castVote(id, "up")}><img className = "VoteButton" src={upvoteIcon}/></button> {vote_count}<button onClick={() => castVote(id, "down")}><img className = "VoteButton" src={downvoteIcon}/></button></h3>
       </Link>
+        <h3><button onClick={() => castVote(id, "up")}><img className = "VoteButton" src={upvoteIcon}/></button> {vote_count}<button onClick={() => castVote(id, "down")}><img className = "VoteButton" src={downvoteIcon}/></button></h3>
     </div>
   );
 }
